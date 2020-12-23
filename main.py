a20be37daf7a9897b48e9e3729124c194a000fa2
Filip Wasik <filipwasik@icloud.com>
2020-12-23 15:27:36 +0100

    update

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import pygame
 import sys
 
 
-# def create_pipe():
-#     new_pipe = pipe_surface.get_rect(midtop=(width / 2, height / 2))
-#     return new_pipe
+def create_pipe():
+    new_pipe = pipe_surface.get_rect(midtop=(int(width / 2), int(height / 2)))
+    return new_pipe
+
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        screen.blit(pipe_surface, pipe)
 
 
 pygame.init()
@@ -14,23 +25,29 @@ width = 400
 height = 600
 gravity = 0.15
 bird_movement = 0
+floor_height = 50
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-floor_height = 50
-# creates a surface with the same size as the window
+# Background
 background = pygame.Surface((width, height))
-# creates a rectangle on the surface
 pygame.draw.rect(background, (0, 0, 0), (0, 0, width, height))
 
+# Floor
 floor_surface = pygame.Surface((width, floor_height))
 pygame.draw.rect(floor_surface, (255, 200, 0), (0, 0, width, floor_height))
 
-bird_surface = pygame.image.load('assets/yellowbird-midflap.png').convert()
+# Bird
+# bird_surface = pygame.image.load('assets/yellowbird-midflap.png').convert()
+bird_surface = pygame.Surface((34, 24))
+bird_surface.fill((255, 0, 0))
 bird_rect = bird_surface.get_rect(center=(100, int(height / 2)))
 
-# pipe_surface = pygame.Surface(width, height)
+# Pipe
+# pipe_surface = pygame.image.load('assets/pipe-green.png').convert()
+pipe_surface = pygame.Surface((50, 100))
+pipe_surface.fill((0, 255, 0))
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1000)
@@ -44,16 +61,21 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 7
-        # if event.type == SPAWNPIPE:
-        #     pipe_list.append(create_pipe)
+        if event.type == SPAWNPIPE:
+            pipe_list.append(create_pipe())
 
     # puts the surface onto the canvas
     screen.blit(background, (0, 0))
     screen.blit(floor_surface, (0, height - floor_height))
 
+    # Bird
     bird_movement += gravity
     bird_rect.centery += int(bird_movement)
     screen.blit(bird_surface, bird_rect)
+
+    # Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
 
     pygame.display.update()
     clock.tick(120)
