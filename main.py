68e2a1d9a2a87efa7381d97a47d5576cd016b29f
Filip Wasik <filipwasik@icloud.com>
2020-12-27 03:46:02 +0100

    c

--- main.py
+++ main.py
@@ -61,33 +61,31 @@ def update_high_score(score, high_score):
 
 
 def get_high_score():
-    # Default high score
     high_score = 0
 
-    # Try to read the high score from a file
     try:
         high_score_file = open("high_score.txt", "r")
         high_score = int(high_score_file.read())
         high_score_file.close()
-        print("The high score is", high_score)
+
     except IOError:
         # Error reading file, no high score
         print("There is no high score yet.")
+
     except ValueError:
         # There's a file there, but we don't understand the number.
-        print("I'm confused. Starting with no high score.")
+        print("Starting with no high score.")
 
     return high_score
 
 
 def save_high_score(new_high_score):
     try:
-        # Write the file to disk
         high_score_file = open("high_score.txt", "w")
         high_score_file.write(str(new_high_score))
         high_score_file.close()
+
     except IOError:
-        # Hm, can't write it.
         print("Unable to save the high score.")
 
 
